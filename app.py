aa72da7c7a9d38e54e1c91559f338cc8187777c9
Scott Pierce <ddrscott@gmail.com>
2024-09-27 10:23:25 -0500

    Reorganize so sidebar processing comes first.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from openai import OpenAI
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 import unstructured
 
 st.title("Chat Doc")
-
-client = OpenAI()
 
 default_system_message = {"role": "system", "content": "You are a helpful assistant."}
 
@@ -20,24 +18,6 @@ if "messages" not in st.session_state:
 if "system_message" not in st.session_state:
     st.session_state["system_message"] = "You are a helpful assistant."
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        messages = [st.session_state.system_message] + st.session_state.messages
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=messages,
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
 
 with st.sidebar:
     uploaded_file = st.file_uploader("Choose a file, less than 5mb")
@@ -60,3 +40,29 @@ with st.sidebar:
         st.write("upload a file to start chatting about it.")
         st.session_state.system_message = default_system_message
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    from openai import AzureOpenAI
+
+    llm = AzureOpenAI(
+        api_key=os.getenv('OPENAI_API_KEY'),
+        api_version="2024-02-01",
+        azure_endpoint=os.getenv('OPENAI_API_BASE')
+    )
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        messages = [st.session_state.system_message] + st.session_state.messages
+        stream = llm.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=messages,
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
